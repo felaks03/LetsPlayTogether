10d148c729239a632434f1e9225d3ce2b6e8c272
felaks03 <felixruizdelacuesta1@gmail.com>
2026-01-16 14:18:55 +0100

    Feat: Created database

---
+++ app-api/src/config/database.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/letplaytogether";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoUri);
+    console.log("MongoDB conectado correctamente");
+  } catch (error) {
+    console.error("Error conectando a MongoDB:", error);
+    process.exit(1);
+  }
+};
