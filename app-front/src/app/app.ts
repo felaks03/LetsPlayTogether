f1aeae8ce9b65d9a0284ddba16c1e38a98ca9201
felaks03 <felixruizdelacuesta1@gmail.com>
2026-01-16 13:45:39 +0100

    Feat: everything running propertly

--- app-front/src/app/app.ts
+++ app-front/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('app-front-temp');
+  protected readonly title = signal('app-front');
 }
