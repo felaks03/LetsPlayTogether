a3485421284a7cf15e494442659eb7727e4393f1
felaks03 <felixruizdelacuesta1@gmail.com>
2026-01-22 17:29:53 +0100

    Update database and server config

--- app-api/src/server.ts
+++ app-api/src/server.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 const port = process.env.PORT || 3000;
 const mongoUri =
-  process.env.MONGO_URI || "mongodb:
+  process.env.MONGO_URI || "mongodb://localhost:27017/letplaytogether";
 
 const connectDB = async () => {
   try {
